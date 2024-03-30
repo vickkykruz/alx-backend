8a29ffe4018cc3e1ce4d747bd076bd09a273c8b3
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-30 11:20:04 +0100

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,4 +6,10 @@ parameters.
 """
 
 
-def 
+def index_range(page: int, page_size: int) -> tuple:
+    """ This function return a tuple size two containing a start index and an
+        end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
