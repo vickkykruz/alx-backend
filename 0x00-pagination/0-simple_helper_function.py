40c5f974874410e8ecd5311f5eeb3afe63ef7662
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-03-30 11:10:08 +0100

    Upload

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" This is a module that create a function and the function should return a
+tuple of size two containing a start index and an end index corresponding to
+the range of indexes to return in a list for those particular pagination
+parameters.
+"""
+
+
+def 
