1ffda86729d3dee3e60f5f01424f72443f681a84
Edward Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-03 10:37:48 +0000

    fix code

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -8,38 +8,35 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """LRUCache class"""
+    """ LRUCache class """
 
     def __init__(self):
-        """Initialize"""
+        """ Initialize LRUCache """
         super().__init__()
-        self.lru_queue = []
-
-    def print_cache(self):
-        """Print the cache"""
-        for key in self.lru_queue:
-            print("{}: {}".format(key, self.cache_data[key]))
-
-    def update_lru_queue(self, key):
-        """Update LRU queue"""
-        if key in self.lru_queue:
-            self.lru_queue.remove(key)
-        self.lru_queue.append(key)
-        if len(self.lru_queue) > self.MAX_ITEMS:
-            least_recently_used = self.lru_queue.pop(0)
-            print("DISCARD:", least_recently_used)
-            del self.cache_data[least_recently_used]
+        self.order = []
 
     def put(self, key, item):
-        """Add an item in the cache"""
+        """ Add an item to the cache """
         if key is None or item is None:
             return
+
+        # If cache is full, discard the least recently used item
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key = self.order.pop(0)
+            del self.cache_data[lru_key]
+            print(f"DISCARD: {lru_key}")
+
         self.cache_data[key] = item
-        self.update_lru_queue(key)
+        if key in self.order:
+            self.order.remove(key)
+        self.order.append(key)
 
     def get(self, key):
-        """Get an item by key"""
+        """ Get an item from the cache """
         if key is None or key not in self.cache_data:
             return None
-        self.update_lru_queue(key)
+
+        # Move the accessed key to the end to mark it as most recently used
+        self.order.remove(key)
+        self.order.append(key)
         return self.cache_data[key]
