409b79e62c3d2e539ad693b5eba3250c0925ad03
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-09 12:37:43 +0100

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" This is a module run the index page on task 0 """
+
+
+from flask import Flask, render_template
+import flask_babel
+
+
+app = Flask(__name__)
+babel = flask_babel.Babel(app)
+
+
+class Config:
+    """ This is a Config class that handle the babel configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """ This is a function that render index page """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
