248f6201f015b3c69708dccb3b031f5292ddc204
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-09 12:18:39 +0100

    fix pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,7 +11,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ This is a function that render index page """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
