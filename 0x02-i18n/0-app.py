6add20060919e1d955e4d363dbab2e24092865e8
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-09 12:15:54 +0100

    Correction

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,23 +6,12 @@ import secrets
 from flask import Flask, render_template
 
 
-def create_application():
-    """ This is a function that create the application """
-
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = secrets.token_hex(16)
+app = Flask(__name__)
 
 
-    @app.route("/")
-    def index_page():
-        """ This is a function that render the index template """
-        return render_template("index.html")
+@app.route('/')
+def index():
+    return render_template('0-index.html')
 
-    return app
-
-
-if __name__ == "__main__":
-    # call the function
-    app = create_application()
-
-    app.run(host="0.0.0.0", port="5000", debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
