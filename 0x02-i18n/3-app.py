c3773bec5c418cde7b0242880e923387fd856df7
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-04-09 13:13:15 +0100

    Upload

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" This is a module run the index page on task 0 """
+
+
+from flask import Flask, render_template, request
+import flask_babel
+
+
+app = Flask(__name__)
+babel = flask_babel.Babel(app)
+
+
+class Config:
+    """ This is a Config class that handle the babel configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """ This is a function that return the locale via request """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ This is a function that render index page """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
