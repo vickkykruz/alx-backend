91813e0590779754e00d6f96af1329663bdc1d1f
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-05-06 15:37:55 +0000

    Node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,17 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.on('error', (err) => console.log(`Redis client not connected to the server: ${err}`));
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe(channel);
+    client.quit();
+  }
+});
