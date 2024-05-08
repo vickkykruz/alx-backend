272c00bd8574f4f2aeb31f46e86c14868f14e993
Victor Chukwuemeka <onwuegbuchulemvic02@gmail.com>
2024-05-08 08:04:41 +0100

    Track progress and errors with Kue: Create the Job creator

---
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,72 @@
+import kue from 'kue';
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account',
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account',
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account',
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account',
+  },
+];
+
+// Create the query
+const queue = kue.createQueue();
+
+// Loop through the jobs
+for (const job of jobs) {
+  // Create a queue for each job
+  const newJob = queue.create('push_notification_code_2', job).save((err) => {
+    if (!err) console.log(`Notification job created: ${newJob.id}`);
+  });
+
+  // Indicate the following
+  newJob.on('complete', () => {
+    console.log(`Notification job ${newJob.id} completed`);
+  });
+
+  newJob.on('failed', (err) => {
+    console.log(`Notification job ${newJob.id} failed: ${err}`);
+  });
+
+  newJob.on('progress', (progress) => {
+    console.log(`Notification job ${newJob.id} ${progress}% complete`);
+  });
+}
